6e0268337a6adc4777da5288cc3299e48fc34bd3
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-09-27 14:42:06 +0200

    Finished 1.2.2

--- MGS.m
+++ MGS.m
@@ -2,26 +2,26 @@
 
 function [Q,R] = MGS(A)
     if ~ismatrix(A)
-        disp('Input is not a matrix')
+        disp('Input is not a matrix');
         return 
     end
 
-    [m,n] = size(A)
+    [m,n] = size(A);
     if n > m
-        disp('More columns than rows.')
+        disp('More columns than rows.');
         return
     end
 
-    V = A
-    Q = zeros(m,n)
-    R = zeros(n)
+    V = A;
+    Q = zeros(m,n);
+    R = zeros(n);
 
     for j = 1:n
         for i = 1:j-1
-            R(i,j) = dot(Q(:,i), V(:,j))
-            V(:,j) = V(:,j) - R(i,j)*Q(:,i)
+            R(i,j) = dot(Q(:,i), V(:,j));
+            V(:,j) = V(:,j) - R(i,j)*Q(:,i);
         end
-        R(j,j) = norm(V(:,j))
-        Q(:,j) = V(:,j) / R(j,j)
+        R(j,j) = norm(V(:,j));
+        Q(:,j) = V(:,j) / R(j,j);
     end
 end
--- MGS_diff.m
+++ MGS_diff.m
@@ -2,26 +2,26 @@
 
 function [Q,R] = MGS_diff(A)
     if ~ismatrix(A)
-        disp('Input is not a matrix')
+        disp('Input is not a matrix');
         return 
     end
 
     [m,n] = size(A)
-    if n > m
-        disp('More columns than rows.')
+    if n > m;
+        disp('More columns than rows.');
         return
     end
 
-    V = A
-    Q = zeros(m,n)
-    R = zeros(n)
+    V = A;
+    Q = zeros(m,n);
+    R = zeros(n);
 
     for i = 1:n
-        R(i,i) = norm(V(:,i))
-        Q(:,i) = V(:,i) / R(i,i)
+        R(i,i) = norm(V(:,i));
+        Q(:,i) = V(:,i) / R(i,i);
         for j = i+1:n
-            R(i,j) = dot(Q(:,i), V(:,j))
-            V(:,j) = V(:,j) - R(i,j)*Q(:,i)
+            R(i,j) = dot(Q(:,i), V(:,j));
+            V(:,j) = V(:,j) - R(i,j)*Q(:,i);
         end
     end
 end
